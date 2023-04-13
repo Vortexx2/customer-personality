fe5e307847ee191a24c787202e930bb93afdb2e4
Vortexx2 <janmayshuklay@gmail.com>
2023-04-13 18:06:22 +0530

    Added saving roc and confusion matrix

--- main_train.py
+++ main_train.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Literal
 
 import os
 import numpy as np
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 from scipy import stats as ss
 import seaborn as sns
 from sklearn import model_selection, linear_model, metrics, preprocessing
+from sklearn.metrics import roc_auc_score, roc_curve
 import sklearn
 # IMPORTS ABOVE
 
@@ -22,7 +23,7 @@ def load_and_process_data(path: str):
 
 
 def get_accuracy(preds: np.ndarray, y: np.ndarray):
-  num_correct = np.equal(preds, y, dtype=np.int16).mean()
+  num_correct = np.equal(preds, y).mean()
 
   return num_correct
 
@@ -36,11 +37,63 @@ def train(X_train: np.ndarray, y_train: np.ndarray, model: str, train_ratio: flo
 
   clf = clf.fit(X_train, y_train)
 
+  return clf
 
-def main(path: str, model: str, train_ratio: float = 0.8):
+
+def save_confusion(y_test: np.ndarray, test_preds: np.ndarray, file_path: str):
+  """
+    Creates and saves a confusion matrix in a folder indicated by save_path under name `confusion.png`.
+  """
+  fig, ax = plt.subplots()
+  confusion_matrix = metrics.confusion_matrix(y_test, test_preds)
+  sns.heatmap(confusion_matrix, fmt='3d', annot=True, ax=ax)
+
+  fig.savefig(file_path)
+  return confusion_matrix
+
+
+def save_roc_curve(test_probs: np.ndarray, y_test: np.ndarray, file_path: str):
+  """
+    Creates a ROC curve and saves it to `file_path`.
+  """
+  # generate a no skill prediction (majority class)
+  ns_probs = [0 for _ in range(len(y_test))]
+
+  # keep probabilities for the positive outcome only
+  test_probs = test_probs[:, 1]
+  # calculate scores
+  ns_auc = roc_auc_score(y_test, ns_probs)
+  lr_auc = roc_auc_score(y_test, test_probs)
+  # summarize scores
+  print('No Skill: ROC AUC=%.3f' % (ns_auc))
+  print('Logistic: ROC AUC=%.3f' % (lr_auc))
+  # calculate roc curves
+  ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
+  lr_fpr, lr_tpr, _ = roc_curve(y_test, test_probs)
+
+  fig, ax = plt.subplots()
+  # plot the roc curve for the model
+  ax.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+  ax.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+  # axis labels
+  ax.set_xlabel('False Positive Rate')
+  ax.set_ylabel('True Positive Rate')
+  # show the legend
+  ax.legend()
+
+  fig.savefig(file_path)
+
+
+def main(csv_path: str, model: Literal['logistic'], save_path: str, train_ratio: float = 0.8):
+  """
+    csv_path: path to csv with extracted features
+    model: One of ['logistic']
+    save_path: Folder where model related files will be saved
+  """
   assert 0 < train_ratio < 1, "Enter valid train ratio"
+  assert model in ['logistic'], f"Enter valid `model`. Invalid `model` {model}"
 
-  X, y = load_and_process_data(path)
+  X, y = load_and_process_data(csv_path)
   X, y = X.to_numpy(), y.to_numpy()
 
   scaler = preprocessing.StandardScaler()
@@ -49,11 +102,26 @@ def main(path: str, model: str, train_ratio: float = 0.8):
   X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=1 - train_ratio, random_state=42,
                                                                       shuffle=True, stratify=y)
 
-  clf = train(X, y, 'logistic', train_ratio=0.8)
+  print(
+      f"No. training examples: {len(X_train)}, No. testing examples: {len(X_test)}")
+
+  clf = train(X, y, model, train_ratio=0.8)
   train_preds = clf.predict(X_train)
   test_preds = clf.predict(X_test)
 
-  train_accuracy, test_accuracy = get_accuracy(train_preds, y_train), get_accuracy(test_preds, y_test)
+  train_accuracy, test_accuracy = get_accuracy(
+      train_preds, y_train), get_accuracy(test_preds, y_test)
   print(f"Train accuracy: {train_accuracy}, Test accuracy: {test_accuracy}")
 
-  
+  save_confusion(y_test, test_preds, os.path.join(save_path, 'confusion.png'))
+  print(f"Confusion Matrix saved at {save_path}")
+
+  test_probs = clf.predict_proba(X_test)
+  save_roc_curve(test_probs, y_test, os.path.join(save_path, 'roc-curve.png'))
+
+
+CSV_PATH = 'data/processed/extracted.csv'
+MODEL = 'logistic'
+SAVE_PATH = 'models/logistic'
+os.makedirs(SAVE_PATH, exist_ok=True)
+main(CSV_PATH, MODEL, SAVE_PATH, train_ratio=0.8)
